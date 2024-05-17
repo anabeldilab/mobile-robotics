04c8e760ac59102ae89cb49e60849af3f0ff5c37
anabeldilab <alu0101206011@ull.edu.es>
2024-05-18 00:11:02 +0200

    Refactor: Added KheperaSupervisor class

--- webots_projects/final_project/controllers/final_project/final_project.py
+++ webots_projects/final_project/controllers/final_project/final_project.py
@@ -8,7 +8,9 @@ Author: Anabel Díaz Labrador
 Control del robot Khepera IV en Webots usando Supervisor.
 """
 
-from src.movement import init_devices_robot, wall_follow, TIME_STEP, turn_gyro, move_forward
+#from src.movement import init_devices_robot, wall_follow, TIME_STEP, turn_gyro, move_forward
+
+from src.robot.KheperaSupervisor import KheperaSupervisor
 
 
 def main():
@@ -16,18 +18,21 @@ def main():
     Función principal para controlar el robot.
     """
     # Activamos los dispositivos necesarios y obtenemos referencias a ellos.
-    robot, left_wheel, right_wheel, ir_sensor_list, _, _, camera, gyro = init_devices_robot(
-        TIME_STEP
-    )
+    #robot, left_wheel, right_wheel, ir_sensor_list, _, _, camera, gyro = init_devices_robot(
+    #    TIME_STEP
+    #)
 
     # wall follow
     #wall_follow(robot, left_wheel, right_wheel, ir_sensor_list)
-    turn_gyro(robot, left_wheel, right_wheel, gyro, 1)
-    turn_gyro(robot, left_wheel, right_wheel, gyro, 1)
-    turn_gyro(robot, left_wheel, right_wheel, gyro, 1)
-    turn_gyro(robot, left_wheel, right_wheel, gyro, 1, direction="right")
-    turn_gyro(robot, left_wheel, right_wheel, gyro, 1, direction="right")
-    turn_gyro(robot, left_wheel, right_wheel, gyro, 1, direction="right")
+    #turn_gyro(robot, left_wheel, right_wheel, gyro, 1)
+    #turn_gyro(robot, left_wheel, right_wheel, gyro, 1)
+    #turn_gyro(robot, left_wheel, right_wheel, gyro, 1)
+    #turn_gyro(robot, left_wheel, right_wheel, gyro, 1, direction="right")
+    #turn_gyro(robot, left_wheel, right_wheel, gyro, 1, direction="right")
+    #turn_gyro(robot, left_wheel, right_wheel, gyro, 1, direction="right")
+
+    khepera_supervisor = KheperaSupervisor()
+    khepera_supervisor.wall_follow()
 
 
 
--- webots_projects/final_project/controllers/final_project/src/motion/movement.py
+++ webots_projects/final_project/controllers/final_project/src/motion/movement.py
@@ -11,7 +11,11 @@ Control of the Khepera IV robot's movement in Webots using Supervisor.
 import math
 from controller import Robot
 from controller import Supervisor
-from .mapping import create_map, display_map, update_map, fill_map, MAP_SIZE
+from src.map.mapping import create_map, display_map, update_map, fill_map, MAP_SIZE
+
+
+TIME_STEP = 32  # milliseconds
+
 
 ORIENTATIONS = {
     "N": [[0, -1], [-1, 0]],
@@ -20,121 +24,8 @@ ORIENTATIONS = {
     "W": [[-1, 0], [0, 1]],
 }
 
-# Maximum wheel speed supported by the robot (khepera4).
-MAX_SPEED = 47.6
-# Default speed for this behavior.
-CRUISE_SPEED = 4
-# Default time step for the controller.
-TIME_STEP = 32
-
-# Names of the infrared-based distance sensors.
-INFRARED_SENSORS_NAMES = [
-    "rear left infrared sensor",
-    "left infrared sensor",
-    "front left infrared sensor",
-    "front infrared sensor",
-    "front right infrared sensor",
-    "right infrared sensor",
-    "rear right infrared sensor",
-    "rear infrared sensor",
-]
-
-
-def enable_distance_sensors(robot, time_step, sensor_names):
-    """
-    Activate and retrieve distance sensors.
-
-    Parameters:
-    - robot: instance of the robot.
-    - time_step: update interval for sensors/actuators in milliseconds.
-    - sensor_names: list of sensor names to activate.
-
-    Returns:
-    - dict: Dictionary with activated distance sensors, ordered as in the sensor_names list.
-    """
-    sensor_dict = {}
-
-    for name in sensor_names:
-        sensor_dict[name] = robot.getDevice(name)
-        sensor_dict[name].enable(time_step)
-
-    return sensor_dict
-
-
-def init_devices(time_step):
-    """
-    Set up and retrieve necessary devices.
-
-    Parameters:
-    - time_step: default update interval in milliseconds for sensors/actuators.
-
-    Returns:
-    - tuple: Contains instances of the robot, wheel motors, infrared sensors, position sensors, and
-    camera.
-    """
-    robot = Supervisor()
-
-    left_wheel = robot.getDevice("left wheel motor")
-    right_wheel = robot.getDevice("right wheel motor")
-    left_wheel.setPosition(float("inf"))
-    right_wheel.setPosition(float("inf"))
-    left_wheel.setVelocity(0)
-    right_wheel.setVelocity(0)
-
-    ir_sensor_list = enable_distance_sensors(robot, time_step, INFRARED_SENSORS_NAMES)
-
-    camera = robot.getDevice("camera")
-    camera.enable(time_step * 10)
-
-    pos_l = robot.getDevice("left wheel sensor")
-    pos_r = robot.getDevice("right wheel sensor")
-    pos_l.enable(time_step)
-    pos_r.enable(time_step)
-    
-    gyro = robot.getDevice('gyro')
-    gyro.enable(TIME_STEP)
-
-    return robot, left_wheel, right_wheel, ir_sensor_list, pos_l, pos_r, camera, gyro
-
-
-def init_devices_robot(time_step):
-    """
-    Set up and retrieve necessary devices.
-
-    Parameters:
-    - time_step: default update interval in milliseconds for sensors/actuators.
-
-    Returns:
-    - tuple: Contains instances of the robot, wheel motors, infrared sensors, position sensors, and
-    camera.
-    """
-    robot = Robot()
-
-    left_wheel = robot.getDevice("left wheel motor")
-    right_wheel = robot.getDevice("right wheel motor")
-    left_wheel.setPosition(float("inf"))
-    right_wheel.setPosition(float("inf"))
-    left_wheel.setVelocity(0)
-    right_wheel.setVelocity(0)
-
-    ir_sensor_list = enable_distance_sensors(robot, time_step, INFRARED_SENSORS_NAMES)
-
-    camera = robot.getDevice("camera")
-    camera.enable(time_step * 10)
-
-    pos_l = robot.getDevice("left wheel sensor")
-    pos_r = robot.getDevice("right wheel sensor")
-    pos_l.enable(time_step)
-    pos_r.enable(time_step)
-    
-    gyro = robot.getDevice('gyro')
-    gyro.enable(TIME_STEP)
-
-    return robot, left_wheel, right_wheel, ir_sensor_list, pos_l, pos_r, camera, gyro
-
-
 def move_forward(
-    robot, left_wheel, right_wheel, distance, front_ir, speed=CRUISE_SPEED
+    robot, left_wheel, right_wheel, distance, front_ir, speed=47.6
 ):
     """
     Move the robot forward a specific distance using closed-loop control.
@@ -294,7 +185,7 @@ def get_target_orientation(khepera_node, direction="left"):
 
 
 def correct_trajectory(
-    robot, left_wheel, right_wheel, left_ir, right_ir, back_ir, speed=CRUISE_SPEED
+    robot, left_wheel, right_wheel, left_ir, right_ir, back_ir, speed=47.6
 ):
     """
     Correct the trajectory of the robot based on the IR sensors.
@@ -349,7 +240,7 @@ def turn_tolerance(current, target, tolerance):
     )
 
 
-def turn(robot, left_wheel, right_wheel, speed=CRUISE_SPEED, direction="left"):
+def turn(robot, left_wheel, right_wheel, speed=47.6, direction="left"):
     """
     Rotate the robot a specified number of degrees.
 
@@ -387,7 +278,7 @@ def turn(robot, left_wheel, right_wheel, speed=CRUISE_SPEED, direction="left"):
     right_wheel.setVelocity(0)
 
 
-def wall_follow(robot, left_wheel, right_wheel, ir_sensor_list, speed=CRUISE_SPEED):
+def wall_follow(robot, left_wheel, right_wheel, ir_sensor_list, speed=47.6):
     """
     Implements a wall following algorithm to map the environment and navigate along walls.
 
@@ -474,7 +365,7 @@ def wall_follow(robot, left_wheel, right_wheel, ir_sensor_list, speed=CRUISE_SPE
     display_map(env_map)
 
 
-def turn_gyro(robot, left_wheel, right_wheel, gyro, speed=CRUISE_SPEED, direction="left"):
+def turn_gyro(robot, left_wheel, right_wheel, gyro, speed=47.6, direction="left"):
     """
     Rotate the robot 90 degrees to the left or right.
     
